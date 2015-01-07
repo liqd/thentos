5026adac5717592e60f2ec969786a6733b3b95bb
Matthias Fischmann <mf@zerobuzz.net>
2015-01-07 16:48:59 +0100

    Fix: debug helpers.

--- tests/TestMain.hs
+++ tests/TestMain.hs
@@ -20,6 +20,7 @@ import Control.Monad.State (liftIO)
 import Data.Acid (AcidState, openLocalStateFrom, closeAcidState)
 import Data.Data (Proxy(Proxy))
 import Data.Functor.Infix ((<$>))
+import Data.IORef (IORef, newIORef, readIORef, writeIORef)
 import Data.String.Conversions (LBS, SBS, cs)
 import Data.Thyme (getCurrentTime)
 import Filesystem (removeTree)
@@ -28,7 +29,7 @@ import LIO.DCLabel (DCLabel, (%%))
 import LIO (LIOState(LIOState), evalLIO)
 import Network.HTTP.Types.Header (Header)
 import Network.HTTP.Types.Method (Method)
-import Network.Wai (Application, StreamingBody, requestMethod, requestBody, pathInfo, requestHeaders)
+import Network.Wai (Application, StreamingBody, requestMethod, requestBody, strictRequestBody, pathInfo, requestHeaders)
 import Network.Wai.Internal (Response(ResponseFile, ResponseBuilder, ResponseStream, ResponseRaw))
 import Network.Wai.Test (Session, SRequest(SRequest),
     runSession, request, srequest, setPath, defaultRequest, simpleStatus, simpleBody)
@@ -36,7 +37,6 @@ import Servant.Server (serve)
 import Test.Hspec (hspec, describe, it, before, after, shouldBe, shouldThrow,
     anyException, shouldSatisfy)
 import Text.Show.Pretty (ppShow)
-import System.IO.Unsafe (unsafePerformIO)
 
 import qualified Data.Aeson as Aeson
 import qualified Network.HTTP.Types.Status as C
@@ -112,7 +112,7 @@ main = hspec $ do
         liftIO $ Aeson.decode' (simpleBody response1) `shouldBe` Just [UserId 0, UserId 1]
 
     describe "POST /user" $
-      it "succeeds" $ \ (db, testServer) -> (debugRunSession True testServer) $ do
+      it "succeeds" $ \ (db, testServer) -> (debugRunSession False testServer) $ do
         response2 <- srequest $ mkSRequest "POST" "/user" [] (Aeson.encode $ User "1" "2" "3" [] Nothing)
         liftIO $ C.statusCode (simpleStatus response2) `shouldBe` 201
 
@@ -156,21 +156,36 @@ mkSRequest method path headers body = SRequest req body
     req = setPath defaultRequest { requestMethod = method, requestHeaders = headers } path
 
 -- | Like `runSession`, but with re-ordered arguments, and with an
--- extra debug-output flag.
+-- extra debug-output flag.  It's not a pretty function, but it helps
+-- with debugging, and it is not intended for production use.
 debugRunSession :: Bool -> Application -> Network.Wai.Test.Session a -> IO a
 debugRunSession debug application session = runSession session (wrapApplication debug)
   where
     wrapApplication :: Bool -> Application
     wrapApplication False = application
     wrapApplication True = \ request respond -> do
-      putStrLn $ showRequest request
-      application request (\ response -> putStrLn (showResponse response)  >> respond response)
+      (requestRendered, request') <- showRequest request
+      print requestRendered
+      application request' (\ response -> putStrLn (showResponse response)  >> respond response)
 
-    showRequest request = showRequestHeader ++ ppShow request ++ body
+    showRequest request = do
+        body :: LBS <- strictRequestBody request
+        bodyRef :: IORef Bool <- newIORef False
+
+        let memoBody = do
+              toggle <- readIORef bodyRef
+              writeIORef bodyRef $ not toggle
+              return $ if toggle then "" else cs body
+
+        let  showRequestHeader = "\n=== REQUEST ==========================================================\n"
+
+             show_ :: LBS -> String
+             show_ body = showRequestHeader ++ ppShow request ++ "\nbody:" ++ show body ++ "\n"
+
+             request' = request { requestBody = memoBody }
+
+        return (show_ body, request')
       where
-        showRequestHeader = "\n=== REQUEST ==========================================================\n"
-
-        body = "\nbody:" ++ show ((cs . unsafePerformIO . requestBody $ request) :: LBS) ++ "\n"
 
     showResponse response = showResponseHeader ++ show_ response
       where
