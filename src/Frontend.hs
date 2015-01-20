4b64b53b2f803a2f28f072048620226b7998af9d
Florian Hartwig <florian.j.hartwig@gmail.com>
2015-01-20 12:11:41 +0100

    Reimplement serveSnaplet to prevent if from trying to parse cmd line args

--- src/Frontend.hs
+++ src/Frontend.hs
@@ -12,7 +12,7 @@ import Data.ByteString (ByteString)
 import Snap.Blaze (blaze)
 import Snap.Core (Snap, route)
 import Snap.Http.Server (httpServe, defaultConfig, setPort)
-import Snap.Snaplet (Snaplet, SnapletInit, snapletValue, makeSnaplet, nestSnaplet, serveSnaplet, addRoutes, Handler)
+import Snap.Snaplet (Snaplet, SnapletInit, snapletValue, makeSnaplet, nestSnaplet, addRoutes, Handler)
 import Snap.Snaplet.AcidState (Acid, acidInitManual, HasAcid(getAcidStore), update)
 import Text.Digestive.Snap (runForm)
 
@@ -20,6 +20,7 @@ import DB (AddUser(AddUser))
 import DB.Protect (thentosPublic)
 import Frontend.Pages (addUserPage, userForm, userAddedPage)
 import Types (DB)
+import Frontend.Util (serveSnaplet)
 
 data FrontendApp = FrontendApp {_db :: Snaplet (Acid DB)}
 
