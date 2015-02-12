fa32b700eaac9caadf6183369a18e6cdb014f642
Matthias Fischmann <mf@zerobuzz.net>
2015-02-12 12:31:54 +0100

    Cleanup: helloworld service: move url config from Site.hs to devel.config.

--- services/helloworld/src/Site.hs
+++ services/helloworld/src/Site.hs
@@ -5,40 +5,34 @@ module Site
   ( app
   ) where
 
-import Control.Applicative
+import Control.Applicative ((<$>), (<*>))
 import Data.ByteString (ByteString)
 import Data.Monoid ((<>))
 import Network.HTTP.Client.Conduit (parseUrl, httpLbs, responseBody, requestHeaders, withManager)
-import Snap
+import Snap (Handler, SnapletInit, makeSnaplet, redirect, redirect', urlEncode, gets, liftIO, getParam, method, Method(GET), ifTop, addRoutes)
 import Snap.Blaze (blaze)
 import Snap.Util.FileServe (serveDirectory)
 import Text.Blaze.Html (Html)
+import Text.Show.Pretty (ppShow)
 
 import qualified Data.ByteString.Char8 as BC
 import qualified Data.Configurator as Configurator
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as HA
 
-data App = App { aServiceId :: ByteString, aServiceKey :: ByteString }
+
+data App = App { aHWConfig :: HWConfig }
 type AppHandler = Handler App App
 
-
--- some hard-wired configuration data.
 data HWConfig =
     HWConfig
       { thentosBackendUrl  :: ByteString
       , thentosFrontendUrl :: ByteString
       , helloWorldUrl      :: ByteString
+      , serviceId          :: ByteString
+      , serviceKey         :: ByteString
       }
   deriving (Eq, Show)
-
-hwConfig :: HWConfig
-hwConfig =
-    HWConfig
-      { thentosBackendUrl  = "http://localhost:8001"
-      , thentosFrontendUrl = "http://localhost:8002"
-      , helloWorldUrl      = "http://localhost:8000"
-      }
 
 
 handleApp :: AppHandler ()
@@ -61,41 +55,56 @@ appPage token sessionMetaData isTokenOk =
             H.p $ H.a H.! HA.href (H.toValue . BC.unpack $ "/logout") $ do
                 H.text "logout"
 
-routes :: ByteString -> [(ByteString, Handler App App ())]
-routes sid = [ ("", ifTop $ redirect "/app")
-             , ("/app", handleApp)
-             , ("/login", helloWorldLogin sid)
-             , ("",     serveDirectory "static")  -- for css and what not.
-             ]
+routes :: HWConfig -> [(ByteString, Handler App App ())]
+routes hwConfig =
+      [ ("",       ifTop $ redirect "/app")
+      , ("/app",   handleApp)
+      , ("/login", helloWorldLogin hwConfig)
+      , ("",       serveDirectory "static")  -- for css and what not.
+      ]
 
 app :: SnapletInit App App
 app = makeSnaplet "app" "A hello-world service for testing thentos." Nothing $ do
-    Just (sid, key) <- liftIO $ loadConfig
-    liftIO . putStrLn $ show (sid, key)
-    addRoutes (routes sid)
-    return $ App sid key
-
+    Just hwConfig <- liftIO $ loadConfig
+    liftIO . putStrLn $ ppShow hwConfig
+    addRoutes (routes hwConfig)
+    return $ App hwConfig
   where
-    loadConfig :: IO (Maybe (ByteString, ByteString))
+    loadConfig :: IO (Maybe HWConfig)
     loadConfig = do
         config <- Configurator.load [Configurator.Required "devel.config"]
-        sid <- Configurator.lookup config "service_id"
-        key <- Configurator.lookup config "service_key"
-        return $ (,) <$> sid <*> key
+        HWConfig <$$>
+            Configurator.lookup config "thentos_backend_url" <**>
+            Configurator.lookup config "thentos_frontend_url" <**>
+            Configurator.lookup config "hello_world_url" <**>
+            Configurator.lookup config "service_id" <**>
+            Configurator.lookup config "service_key"
 
-helloWorldLogin :: ByteString -> Handler App App ()
-helloWorldLogin serviceId =
+    -- function-infix provides (<$$>), but not (<**>).  While waiting
+    -- for somebody to write a package applicative-infix, we define
+    -- these inline, with slightly more specific types.  (NOTE:
+    -- Control.Applicative.<**> exists, but does something else.)
+
+    (<$$>) :: (a -> b) -> IO (Maybe a) -> IO (Maybe b)
+    a <$$> b = do y <- b; return $ a <$> y
+
+    (<**>) :: IO (Maybe (a -> b)) -> IO (Maybe a) -> IO (Maybe b)
+    a <**> b = do x <- a; y <- b; return $ x <*> y
+
+helloWorldLogin :: HWConfig -> Handler App App ()
+helloWorldLogin hwConfig =
     redirect'
-        (thentosFrontendUrl hwConfig <> "/login?sid=" <> serviceId <> "&redirect="
+        (thentosFrontendUrl hwConfig <> "/login?sid=" <> (serviceId hwConfig) <> "&redirect="
             <> urlEncode (helloWorldUrl hwConfig <> "/app?foo=bar"))
         303
 
 tokenOk :: Maybe ByteString -> Handler App App Bool
 tokenOk Nothing = return False
 tokenOk (Just token) = do
-    sid <- gets aServiceId
-    key <- gets aServiceKey
-    let url = thentosBackendUrl hwConfig <> "/session/" <> token
+    hwConfig <- gets aHWConfig
+    let sid = serviceId hwConfig
+        key = serviceKey hwConfig
+        url = thentosBackendUrl hwConfig <> "/session/" <> token
     liftIO . withManager $ do
         initReq <- parseUrl $ BC.unpack url
         let req = initReq
