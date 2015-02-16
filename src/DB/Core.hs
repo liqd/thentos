4e0ad542cc3a244669601d8aae0c20592f847b21
Matthias Fischmann <mf@zerobuzz.net>
2015-02-16 15:53:19 +0100

    Fix: typos.

--- src/DB/Core.hs
+++ src/DB/Core.hs
@@ -103,8 +103,8 @@ showDbError e@(PermissionDenied _ _)             = logger INFO (show e) >> retur
 showDbError e@BadCredentials                     = logger INFO (show e) >> return (401, "unauthorized")
 showDbError BadAuthenticationHeaders             = return (400, "bad authentication headers")
 showDbError ProxyNotAvailable                    = return (404, "proxying not activated")
-showDbError MissingServiceHeader                 = return (404, "headers do not ontain service id")
-showDbError (ProxyNotConfiguredForService sid)   = return (404, "proxy not configured for service" ++ show sid)
+showDbError MissingServiceHeader                 = return (404, "headers do not contain service id")
+showDbError (ProxyNotConfiguredForService sid)   = return (404, "proxy not configured for service " ++ show sid)
 
 
 -- | FIXME: generalize, so we can use this for both Update and Query.
