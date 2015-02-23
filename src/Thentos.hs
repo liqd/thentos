82da41bcac0d9cf73b6ae91c833ba4a655131360
Matthias Fischmann <mf@zerobuzz.net>
2015-02-23 15:58:34 +0100

    Cleanup.

--- src/Thentos.hs
+++ src/Thentos.hs
@@ -17,13 +17,14 @@ module Thentos (main) where
 
 import Control.Concurrent.Async (concurrently)
 import Control.Concurrent.MVar (MVar, newMVar)
-import Control.Exception (finally)
+import Control.Exception (bracket, finally)
 import Control.Monad (void)
 import Crypto.Random (SystemRNG, createEntropyPool, cprgCreate)
 import Data.Acid (AcidState, openLocalStateFrom, createCheckpoint, closeAcidState)
 import Data.Acid.Advanced (query')
 import Data.String.Conversions ((<>))
-import System.Log.Logger (Priority(DEBUG), removeAllHandlers)
+import System.IO (hPutStrLn, stderr)
+import System.Log.Logger (Priority(DEBUG, INFO), removeAllHandlers)
 import Text.Show.Pretty (ppShow)
 
 import Thentos.Config (configLogger, getCommand, Command(..), ThentosConfig(..), BackendConfig(BackendConfig), FrontendConfig(FrontendConfig))
@@ -41,10 +42,10 @@ import qualified Thentos.Backend.Api.Adhocracy3 (runBackend)
 main :: IO ()
 main =
   do
-    putStr "setting up acid-state..."
-    st :: AcidState DB <- openLocalStateFrom ".acid-state/" emptyDB
-    putStrLn " [ok]"
+    let notify :: String -> IO a -> IO a
+        notify msg action = bracket (logger INFO msg) (\ _ -> logger INFO $ msg ++ ": [ok]") (\ _ -> action)
 
+    st :: AcidState DB <- notify "setting up acid-state" $ openLocalStateFrom ".acid-state/" emptyDB
     rng :: MVar SystemRNG <- createEntropyPool >>= newMVar . cprgCreate
 
     configLogger
@@ -62,17 +63,17 @@ main =
                 let backend = case backendConfig config of
                         Nothing -> return ()
                         Just (BackendConfig backendPort) -> do
-                            putStrLn $ "running rest api on localhost:" <> show backendPort <> "."
+                            logger INFO $ "running rest api on localhost:" <> show backendPort <> "."
                             Thentos.Backend.Api.Simple.runBackend backendPort (st, rng, config)
 
                 let frontend = case frontendConfig config of
                         Nothing -> return ()
                         Just (FrontendConfig frontendPort) -> do
-                            putStrLn $ "running frontend on localhost:" <> show frontendPort <> "."
+                            logger INFO $ "running frontend on localhost:" <> show frontendPort <> "."
                             runFrontend "localhost" frontendPort (st, rng, config)
 
                 _ <- createCheckpointLoop st 16000 Nothing
-                putStrLn "Press ^C to abort."
+                hPutStrLn stderr "Press ^C to abort."
                 void $ concurrently backend frontend
 
             RunA3 config -> do
@@ -82,18 +83,14 @@ main =
                 case backendConfig config of
                         Nothing -> error "command `runa3` requires `--runbackend`"
                         Just (BackendConfig backendPort) -> do
-                            putStrLn $ "running a3 rest api on localhost:" <> show backendPort <> "."
-                            putStrLn "Press ^C to abort."
+                            logger INFO $ "running a3 rest api on localhost:" <> show backendPort <> "."
+                            hPutStrLn stderr "Press ^C to abort."
                             Thentos.Backend.Api.Adhocracy3.runBackend backendPort (st, rng, config)
 
     let finalize = do
-            putStr "creating checkpoint and shutting down acid-state..."
-            createCheckpoint st
-            closeAcidState st
-            putStrLn " [ok]"
-
-            putStr "shutting down hslogger..."
-            removeAllHandlers
-            putStrLn " [ok]"
+            notify "creating checkpoint and shutting down acid-state" $
+                createCheckpoint st >> closeAcidState st
+            notify "shutting down hslogger" $
+                removeAllHandlers
 
     run `finally` finalize
