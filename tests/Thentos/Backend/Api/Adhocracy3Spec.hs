811e2a6dadb3936e5e3154d81c474f5758c2bac1
Matthias Fischmann <mf@zerobuzz.net>
2015-02-26 16:11:39 +0100

    Fix: a3 user type json encoding tests.

--- tests/Thentos/Backend/Api/Adhocracy3Spec.hs
+++ tests/Thentos/Backend/Api/Adhocracy3Spec.hs
@@ -68,18 +68,14 @@ spec = do
     describe "Thentos.Backend.Api.Adhocracy3" $ do
         describe "A3UserNoPass" $ do
             it "has invertible *JSON instances" . property $
-                let (===) (Right (A3UserNoPass (UserFormData n "" e)))
+                let (===) (Right (A3UserNoPass (UserFormData n _ e)))
                           (Right (A3UserNoPass (UserFormData n' _ e')))
                               = n == n' && e == e'
                     (===) _ _ = False
                 in \ (A3UserNoPass -> u) -> (Aeson.eitherDecode . Aeson.encode) u === Right u
         describe "A3UserWithPassword" $ do
             it "has invertible *JSON instances" . property $
-                let (===) (Right (A3UserWithPass (UserFormData n "[password hidden]" e)))
-                          (Right (A3UserWithPass (UserFormData n' _ e')))
-                              = n == n' && e == e'
-                    (===) _ _ = False
-                in \ (A3UserWithPass -> u) -> (Aeson.eitherDecode . Aeson.encode) u === Right u
+                \ (A3UserWithPass -> u) -> (Aeson.eitherDecode . Aeson.encode) u == Right u
 
         describe "create user" . before setupTestA3Server . after teardownA3TestServer $ do
             it "works" $
