cebd575ee1e7e43dc9532bc752fb2a7d360539a6
Florian Hartwig <florian.j.hartwig@gmail.com>
2015-03-18 13:42:04 +0100

    Make selenium tests optional
    They are enabled by passing the argument `enableBrowserTests` to the test
    executable. When using cabal, this can be done by calling
    `cabal test --test-option=enableBrowserTests`

--- tests/Spec.hs
+++ tests/Spec.hs
@@ -1,1 +1,25 @@
-{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+import Test.Hspec
+
+import Control.Monad (when)
+import System.Environment (getArgs, withArgs)
+
+import qualified ThentosSpec
+import qualified Thentos.Backend.Api.Adhocracy3Spec
+import qualified Thentos.Backend.Api.SimpleSpec
+import qualified Thentos.FrontendSpec
+import qualified Thentos.TypesSpec
+
+main :: IO ()
+main = do
+    args <- getArgs
+    case args of
+        "enableBrowserTests" : xs -> withArgs xs $ hspec (spec True)
+        _ -> hspec $ spec False
+
+spec :: Bool -> Spec
+spec browserTests = do
+    describe "Thentos" ThentosSpec.spec
+    describe "Thentos.Types" Thentos.TypesSpec.spec
+    describe "Thentos.Backend.Api.Adhocracy3" Thentos.Backend.Api.Adhocracy3Spec.spec
+    describe "Thentos.Backend.Api.Simple" Thentos.Backend.Api.SimpleSpec.spec
+    when browserTests $ describe "Thentos.Frontend" Thentos.FrontendSpec.spec
