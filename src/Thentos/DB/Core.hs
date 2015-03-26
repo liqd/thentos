1daa7ff1ec8f49b90df54c44a1adb540f9970d7d
Matthias Fischmann <mf@zerobuzz.net>
2015-03-26 13:24:34 +0100

    Allow override transaction labels in actions.

--- src/Thentos/DB/Core.hs
+++ src/Thentos/DB/Core.hs
@@ -7,8 +7,10 @@ module Thentos.DB.Core
   ( ThentosClearance(..)
   , ThentosUpdate, ThentosUpdate'
   , runThentosUpdate
+  , runThentosUpdateWithLabel
   , ThentosQuery, ThentosQuery'
   , runThentosQuery
+  , runThentosQueryWithLabel
   , liftThentosQuery
   , returnDb
   , throwDb
@@ -37,7 +39,7 @@ import Control.Monad.State (StateT(StateT), runStateT, get, put)
 import Control.Monad.Trans.Either (EitherT(EitherT), right, runEitherT)
 import Data.Acid (AcidState, Update, Query, createCheckpoint)
 import Data.EitherR (throwT)
-import LIO (canFlowTo)
+import LIO (canFlowTo, glb)
 import LIO.DCLabel (DCLabel(DCLabel), ToCNF, (%%), (/\), (\/), toCNF)
 
 import Thentos.Types
@@ -59,25 +61,87 @@ liftThentosQuery thentosQuery = EitherT $ StateT $ \ state ->
     (, state) <$> runEitherT thentosQuery `runReaderT` state
 
 
--- | FIXME: generalize, so we can use this for both Update and Query.
--- (remove 'runThentosQuery' and 'ThentosQuery' when done.)
+-- | Apply clearance and push 'ThentosUpdate' value down to
+-- acid-state's own 'Update'.  Errors are returned as 'Left' values in
+-- an 'Either'.
+--
+-- This makes it possible to implement transactions as 'ThenotsUpdate'
+-- rather than 'Update', which gives extra error handling and
+-- authorization labelling.  The actual acid-state transactions
+-- ('Update') are constructed in mechanically written transaction
+-- wrappers (see section towards the end of "Thentos.DB.Trans" for
+-- examples).
+--
+--
+-- FUTURE WORK.  Returning errors as 'Either' has two drawbacks:
+--
+-- 1. it results in changelogs that contain 'Left' updates (even
+--    though they do not change anything), and
+-- 2. in the code that runs the transactions, we cannot make use of
+--    monad transformers for error handling and propagation.
+--
+-- There may be ways to avoid this, but the most obvious
+-- one would be to enhance the 'Update' monad with error handling, and
+-- that would require changes new versions of acid-state's 'update',
+-- 'update'', and others.
+--
+-- The benefit would be both in performance (smaller, less redundant
+-- changelog) and in code quality (e.g., no need for 'SafeCopy'
+-- instance for errors).
+--
+-- See also:
+--
+-- - http://www.reddit.com/r/haskell/comments/2re0da/error_handling_in_acidstate/
+-- - http://petterbergman.se/aciderror.html.en
+-- - http://acid-state.seize.it/Error%20Scenarios
+-- - https://github.com/acid-state/acid-state/pull/38
+--
 runThentosUpdate :: forall a . Show a => ThentosClearance -> ThentosUpdate a -> Update DB (Either ThentosError a)
-runThentosUpdate clearance action = do
+runThentosUpdate = runThentosUpdateWithLabel thentosDenied
+
+-- | This is to 'Query' what 'runThentosUpdate' is to 'Update'.
+runThentosQuery :: forall a . Show a => ThentosClearance -> ThentosQuery a -> Query DB (Either ThentosError a)
+runThentosQuery = runThentosQueryWithLabel thentosDenied
+
+-- | Like 'runThentosUpdate', but with override label.
+--
+-- Transactions ('ThentosUpdate', 'ThentosQuery', as in
+-- "Thentos.DB.Trans") are currently the only place where labels can
+-- be attached to where 'ThentosLabel's for authorization can be
+-- assigned to code.  Actions, as in "Thentos.Api", should work
+-- without authorization check if the transactions get the job done,
+-- but sometimes, they need to be able to override the labels set by
+-- transactions.
+--
+-- Example: 'checkPassword' takes a 'UserName' and 'UserPass', looks
+-- up the user, and calls 'verifyPass'.  The user lookup transaction
+-- certainly should be restricted to certain clearance levels, but in
+-- this particular case, we certainly can't expect any clearance level
+-- to be established yet.
+--
+-- Solution: This function takes an extra argument of type
+-- 'ThentosLabel' and check clearance against the 'glb' of that label
+-- and the one set by the transaction.
+runThentosUpdateWithLabel :: forall a . Show a
+      => ThentosLabel -> ThentosClearance -> ThentosUpdate a -> Update DB (Either ThentosError a)
+runThentosUpdateWithLabel overrideLabel clearance action = do
     state <- get
     case runIdentity $ runStateT (runEitherT action) state of
         (Left (ThentosLabeled label (err :: ThentosError)), _) ->
-            checkClearance (show err) clearance label (return $ Left err)
+            checkClearance (show err) clearance (glb overrideLabel label) (return $ Left err)
         (Right (ThentosLabeled label result), state') ->
-            checkClearance (show result) clearance label (put state' >> return (Right result))
+            checkClearance (show result) clearance (glb overrideLabel label) (put state' >> return (Right result))
 
-runThentosQuery :: forall a . Show a => ThentosClearance -> ThentosQuery a -> Query DB (Either ThentosError a)
-runThentosQuery clearance action = do
+-- | This is to 'Query' what 'runThentosUpdateWithLabel' is to 'Update'.
+runThentosQueryWithLabel :: forall a . Show a
+      => ThentosLabel -> ThentosClearance -> ThentosQuery a -> Query DB (Either ThentosError a)
+runThentosQueryWithLabel overrideLabel clearance action = do
     state <- ask
     case runIdentity $ runReaderT (runEitherT action) state of
         Left (ThentosLabeled label (err :: ThentosError)) ->
-            checkClearance (show err) clearance label (return $ Left err)
+            checkClearance (show err) clearance (glb overrideLabel label) (return $ Left err)
         Right (ThentosLabeled label result) ->
-            checkClearance (show result) clearance label (return $ Right result)
+            checkClearance (show result) clearance (glb overrideLabel label) (return $ Right result)
 
 checkClearance :: Monad m => String -> ThentosClearance -> ThentosLabel -> m (Either ThentosError a) -> m (Either ThentosError a)
 checkClearance msg clearance label result =
