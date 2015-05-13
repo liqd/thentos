2e2caa90e7d802c0a502f91f26365902ce3b273f
Matthias Fischmann <mf@zerobuzz.net>
2015-05-13 19:27:51 +0200

    Fix: BuildDocs compiler errors (caused by upgrade to servant-0.4).

--- exec/BuildDocs.hs
+++ exec/BuildDocs.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DataKinds                                #-}
+{-# LANGUAGE MultiParamTypeClasses                    #-}
 {-# LANGUAGE FlexibleInstances                        #-}
 {-# LANGUAGE OverlappingInstances                     #-}
 {-# LANGUAGE OverloadedStrings                        #-}
@@ -32,7 +33,7 @@ import qualified Servant.Docs as Docs
 import Thentos.Backend.Core
 import Thentos.Types
 
-import qualified Thentos.Backend.Api.Adhocracy3 as Adhocracy3
+-- import qualified Thentos.Backend.Api.Adhocracy3 as Adhocracy3
 import qualified Thentos.Backend.Api.Simple as Simple
 
 
@@ -66,10 +67,10 @@ xwriter apiName formatName = do
 --                           (Left bad) -> error $ show bad
 
 xdocs :: ApiName -> Docs.API
-xdocs Api_Simple     = docs (Proxy :: Proxy Simple.App)
-xdocs Api_Adhocracy3 = docs (Proxy :: Proxy Adhocracy3.App)
+xdocs Api_Simple     = docs (Proxy :: Proxy Simple.Api)
+-- xdocs Api_Adhocracy3 = docs (Proxy :: Proxy Adhocracy3.App)
 
-data ApiName = Api_Simple | Api_Adhocracy3
+data ApiName = Api_Simple  -- | Api_Adhocracy3
   deriving (Eq, Enum, Bounded, Read, Show)
 
 data FormatName = Format_Markdown | Format_Html | Format_Docx
@@ -83,13 +84,11 @@ xsystem cmd = do
     return ()
 
 
-instance HasDocs sublayout => HasDocs (Simple.ThentosAuth sublayout) where
-    docsFor Proxy = docsFor (Proxy :: Proxy sublayout)
-
 instance HasDocs sublayout => HasDocs (ThentosAssertHeaders sublayout) where
     docsFor Proxy = docsFor (Proxy :: Proxy sublayout)
 
 
+{-
 -- | (can this be derived entirely?)
 instance ToSample Adhocracy3.A3UserNoPass where
     toSample = Adhocracy3.A3UserNoPass <$> toSample
@@ -114,9 +113,10 @@ instance ToSample Adhocracy3.RequestResult where
 
 instance ToSample Adhocracy3.ContentType where
     toSample = pure $ Adhocracy3.CTUser
+-}
 
-instance ToCapture (Capture "token" SessionToken) where
-    toCapture _ = DocCapture "token" "Session Token"
+instance ToCapture (Capture "token" ThentosSessionToken) where
+    toCapture _ = DocCapture "token" "Thentos Session Token"
 
 instance ToCapture (Capture "token" ServiceSessionToken) where
     toCapture _ = DocCapture "token" "Service Session Token"
@@ -127,71 +127,71 @@ instance ToCapture (Capture "sid" ServiceId) where
 instance ToCapture (Capture "uid" UserId) where
     toCapture _ = DocCapture "uid" "User ID"
 
-instance ToSample Agent where
-    toSample = Just . UserA . UserId $ 0
+instance ToSample Agent Agent where
+    toSample _ = Just . UserA . UserId $ 0
 
-instance ToSample SessionToken where
-    toSample = Just "abde1234llkjh"
+instance ToSample ThentosSessionToken ThentosSessionToken where
+    toSample _ = Just "abde1234llkjh"
 
-instance ToSample [SessionToken] where
-    toSample = Just ["abde1234llkjh", "47202sdfsg"]
+instance ToSample [ThentosSessionToken] [ThentosSessionToken] where
+    toSample _ = Just ["abde1234llkjh", "47202sdfsg"]
 
-instance ToSample UserFormData where
-    toSample = Just $ UserFormData (UserName "Kurt Cobain")
+instance ToSample UserFormData UserFormData where
+    toSample _ = Just $ UserFormData (UserName "Kurt Cobain")
                                    (UserPass "Hunter2")
                                    (UserEmail "cobain@nirvana.com")
 
-instance ToSample UserName where
-    toSample = Just $ UserName "Kurt Cobain"
+instance ToSample UserName UserName where
+    toSample _ = Just $ UserName "Kurt Cobain"
 
-instance ToSample UserEmail where
-    toSample = Just $ UserEmail "cobain@nirvana.com"
+instance ToSample UserEmail UserEmail where
+    toSample _ = Just $ UserEmail "cobain@nirvana.com"
 
-instance ToSample UserId where
-    toSample = Just $ UserId 12
+instance ToSample UserId UserId where
+    toSample _ = Just $ UserId 12
 
-instance ToSample (UserId, UserPass) where
-    toSample = Just (UserId 12, UserPass "geheim")
+instance ToSample (UserId, UserPass) (UserId, UserPass) where
+    toSample _ = Just (UserId 12, UserPass "geheim")
 
-instance ToSample [UserId] where
-    toSample = Just [UserId 3, UserId 7, UserId 23]
+instance ToSample [UserId] [UserId] where
+    toSample _ = Just [UserId 3, UserId 7, UserId 23]
 
-instance ToSample ServiceId where
-    toSample = Just "23t92ege0n"
+instance ToSample ServiceId ServiceId where
+    toSample _ = Just "23t92ege0n"
 
-instance ToSample ServiceKey where
-    toSample = Just "yd090129rj"
+instance ToSample ServiceKey ServiceKey where
+    toSample _ = Just "yd090129rj"
 
-instance ToSample ServiceName where
-    toSample = Just "Evil Corp."
+instance ToSample ServiceName ServiceName where
+    toSample _ = Just "Evil Corp."
 
-instance ToSample ServiceDescription where
-    toSample = Just "don't be evil"
+instance ToSample ServiceDescription ServiceDescription where
+    toSample _ = Just "don't be evil"
 
-instance ToSample [ServiceId] where
-    toSample = Just ["23t92ege0n", "f4ghwgegin0"]
+instance ToSample [ServiceId] [ServiceId] where
+    toSample _ = Just ["23t92ege0n", "f4ghwgegin0"]
 
-instance ToSample (UserId, Timeout) where
-    toSample = (,) <$> toSample <*> pure (Timeout $ fromSeconds (123456.0 :: Double))
+instance ToSample (UserId, Timeout) (UserId, Timeout) where
+    toSample _ = (,) <$> toSample (Proxy :: Proxy UserId) <*> pure (Timeout $ fromSeconds (123456.0 :: Double))
 
-instance ToSample (UserId, ServiceId) where
-    toSample = (,) <$> toSample <*> toSample
+instance ToSample (UserId, ServiceId) (UserId, ServiceId) where
+    toSample _ = (,) <$> toSample (Proxy :: Proxy UserId) <*> toSample (Proxy :: Proxy ServiceId)
 
-instance ToSample ServiceSessionMetadata where
-    toSample = ServiceSessionMetadata <$> toSample
+instance ToSample ServiceSessionMetadata ServiceSessionMetadata where
+    toSample _ = ServiceSessionMetadata <$> toSample (Proxy :: Proxy UserName)
 
-instance ToSample () where
-    toSample = Just ()
+instance ToSample () () where
+    toSample _ = Just ()
 
-instance ToSample Bool where
-    toSample = Just True
+instance ToSample Bool Bool where
+    toSample _ = Just True
 
 -- | cover for tuples whose components have already been given
 -- examples.  if you write an instance for a tuple for two concrete
 -- types, `-XOverlappingInstances` will disregard this instance as
 -- more general.
-instance (ToSample a, ToSample b) => ToSample (a, b) where
-    toSample = (,) <$> toSample <*> toSample
+instance (ToSample a a, ToSample b b) => ToSample (a, b) (a, b) where
+    toSample _ = (,) <$> toSample (Proxy :: Proxy a) <*> toSample (Proxy :: Proxy b)
 
-instance (ToSample a, ToSample b, ToSample c) => ToSample (a, b, c) where
-    toSample = (,,) <$> toSample <*> toSample <*> toSample
+instance (ToSample a a, ToSample b b, ToSample c c) => ToSample (a, b, c) (a, b, c) where
+    toSample _ = (,,) <$> toSample (Proxy :: Proxy a) <*> toSample (Proxy :: Proxy b) <*> toSample (Proxy :: Proxy c)
