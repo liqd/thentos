ce9304f97539db6ce540d24e7582cd6978957d71
Florian Hartwig <florian.j.hartwig@gmail.com>
2015-05-28 15:23:24 +0200

    Capture and log output from sendmail

--- src/Thentos/Smtp.hs
+++ src/Thentos/Smtp.hs
@@ -6,12 +6,16 @@ module Thentos.Smtp
 where
 
 import Control.Applicative ((<$>))
+import Control.Monad (when)
 import Data.Configifier ((>>.))
 import Data.Proxy (Proxy(Proxy))
 import Data.String.Conversions (ST, cs)
-import Network.Mail.Mime (Mail, Address(Address), renderSendMailCustom, simpleMail')
-import System.Log (Priority(DEBUG))
+import Network.Mail.Mime (Mail, Address(Address), sendmailCustomCaptureOutput,
+    simpleMail', renderMail')
+import System.Log (Priority(DEBUG, WARNING))
 import Text.Show.Pretty (ppShow)
+
+import qualified Data.ByteString as SB
 
 import System.Log.Missing
 import Thentos.Config
@@ -20,7 +24,12 @@ import Thentos.Types
 sendMail :: SmtpConfig -> Maybe UserName -> UserEmail -> ST -> ST -> IO ()
 sendMail config mName address subject message = do
     logger DEBUG $ "sending email: " ++ ppShow (address, subject, message)
-    renderSendMailCustom sendmailPath sendmailArgs mail
+    renderedMail <- renderMail' mail
+    (out, err) <- sendmailCustomCaptureOutput sendmailPath sendmailArgs renderedMail
+    when (not $ SB.null out) $
+        logger WARNING $ "sendmail produced output on std out: " ++ cs out
+    when (not $ SB.null err) $
+        logger WARNING $ "sendmail produced output on std err: " ++ cs err
   where
     receiverAddress = Address (fromUserName <$> mName) (fromUserEmail $ address)
     sentFromAddress = buildEmailAddress config
