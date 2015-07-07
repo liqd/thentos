66277551923497560e7a1f3600ce5581f4e2fe0d
Matthias Fischmann <mf@zerobuzz.net>
2015-07-07 11:13:10 +0200

    Cleanup: make MetaSpec accomodate multiple tests.

--- tests/MetaSpec.hs
+++ tests/MetaSpec.hs
@@ -1,22 +1,29 @@
 module MetaSpec where
 
 import Data.IORef (IORef, newIORef, readIORef, writeIORef, modifyIORef)
-import Test.Hspec (Spec, describe, it, before_, shouldBe, runIO)
+import Test.Hspec (Spec, describe, it, before_, shouldBe, runIO, hspec)
 
--- `before` and `after` hooks are run before and after each spec item (see
--- http://hspec.github.io/writing-specs.html ). Illustrative example:
+tests :: IO ()
+tests = hspec spec
 
 spec :: Spec
-spec = do
+spec = hooks
+
+
+-- `before` and `after` hooks are run before and after each spec item (see
+-- http://hspec.github.io/writing-specs.html). Illustrative example:
+
+hooks :: Spec
+hooks = do
     ref <- runIO (newIORef 123)
     describe "all the tests" . before_ (beforeAction ref) $ do
-        spec_foo ref
+        specFoo ref
 
 beforeAction :: IORef Int -> IO ()
 beforeAction ref = writeIORef ref 0
 
-spec_foo :: IORef Int -> Spec
-spec_foo ref = describe "do the test" $ do
+specFoo :: IORef Int -> Spec
+specFoo ref = describe "do the test" $ do
     it "first spec item modifies environment" $ do
         cur <- readIORef ref
         cur `shouldBe` 0
