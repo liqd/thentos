7e531b302cb0d4b20146f2f33fb802bb8ae30773
Matthias Fischmann <mf@zerobuzz.net>
2015-07-14 14:38:25 +0200

    More specific name for a3 api docs file.

--- thentos-adhocracy/exec/BuildDocs.hs
+++ thentos-adhocracy/exec/BuildDocs.hs
@@ -5,4 +5,4 @@ import qualified Thentos.Adhocracy3.Backend.Api.Docs.Simple as Api
 
 
 main :: IO ()
-main = ThentosDocs.makeMain "./docs/generated" [("simple", Api.docs)]
+main = ThentosDocs.makeMain "./docs/generated" [("adhocracy3_proxy_mode", Api.docs)]
