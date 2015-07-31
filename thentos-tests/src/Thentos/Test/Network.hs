a950f5cc2da1c0e097a998e16f3adcc35ad8bb4b
Robert Vollmert <rfvollmert@gmail.com>
2015-07-31 15:18:33 +0200

    Add random socket opening helper.

---
+++ thentos-tests/src/Thentos/Test/Network.hs
@@ -0,0 +1,14 @@
+module Thentos.Test.Network
+where
+
+import Network.Socket --(bind, listen, socket, Family(AF_INET), SocketType(Stream))
+
+
+openTestSocket :: IO (PortNumber, Socket)
+openTestSocket = do
+    s <- socket AF_INET Stream defaultProtocol
+    localhost <- inet_addr "127.0.0.1"
+    bind s (SockAddrInet aNY_PORT localhost)
+    listen s 1
+    port <- socketPort s
+    return (fromIntegral port, s)
