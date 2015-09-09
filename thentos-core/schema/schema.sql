a1d30f59efad7adfc29d891acf0e5d58efbf5aca
Christian Siefkes <christian@siefkes.net>
2015-09-09 15:06:35 +0200

    Add some forgotten PRIMARY KEY and NOT NULL constraints
    Also nicer SQL formatting

--- thentos-core/schema/schema.sql
+++ thentos-core/schema/schema.sql
@@ -1,40 +1,40 @@
 -- All statements in this file should be idempotent.
 CREATE TABLE IF NOT EXISTS "users" (
     id         bigint      PRIMARY KEY,
-    name       text        UNIQUE NOT NULL,
+    name       text        NOT NULL UNIQUE,
     password   text        NOT NULL,
-    email      text        UNIQUE NOT NULL,
+    email      text        NOT NULL UNIQUE,
     confirmed  bool        NOT NULL,
     created    timestamptz NOT NULL DEFAULT now()
 );
 
 CREATE TABLE IF NOT EXISTS "user_confirmation_tokens" (
-    id         bigint      REFERENCES users (id) ON DELETE CASCADE,
-    token      text        UNIQUE NOT NULL
+    id         bigint      NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+    token      text        NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS "password_reset_tokens" (
-    token      text        NOT NULL,
+    token      text        PRIMARY KEY,
     uid        bigint      NOT NULL REFERENCES users (id) ON DELETE CASCADE,
-    timestamp  timestamptz DEFAULT now()
+    timestamp  timestamptz NOT NULL DEFAULT now()
 );
 
 CREATE TABLE IF NOT EXISTS "email_change_tokens" (
-    token      text        NOT NULL,
+    token      text        PRIMARY KEY,
     uid        bigint      NOT NULL REFERENCES users (id) ON DELETE CASCADE,
-    timestamp  timestamptz DEFAULT now(),
+    timestamp  timestamptz NOT NULL DEFAULT now(),
     new_email  text        NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS "user_roles" (
-    uid bigint NOT NULL REFERENCES users (id) ON DELETE CASCADE,
-    role text NOT NULL,
+    uid  bigint NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+    role text   NOT NULL,
     UNIQUE (uid, role)
 );
 
 CREATE TABLE IF NOT EXISTS "services" (
     id          text       PRIMARY KEY,
-    owner       bigint     REFERENCES users (id),
+    owner       bigint     NOT NULL REFERENCES users (id),
     name        text       NOT NULL,
     description text       NOT NULL,
     key         text       NOT NULL
