f023751c4e26a2fd662a090c1bb69e1430d297f0
Matthias Fischmann <mf@zerobuzz.net>
2015-10-04 18:13:06 +0200

    Incremental/watch flag for purescript build script.

--- thentos-core/purescript/build-js.sh
+++ thentos-core/purescript/build-js.sh
@@ -3,6 +3,17 @@
 set -o errexit
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-mkdir -p ../js
-pulp dep install
-pulp build -O --to ../js/thentos.js
+case "$1" in
+    "")
+        mkdir -p ../js
+        pulp dep install
+        pulp build -O --to ../js/thentos.js
+        ;;
+    "-i")
+        pulp --watch build --to ../js/thentos.js
+        ;;
+    *)
+        echo "usage: $0 [-i]" >&2
+        exit 1
+        ;;
+esac
