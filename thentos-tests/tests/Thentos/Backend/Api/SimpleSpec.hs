3ac39838f523fe46cc454e6ecf9384e70f95a0bd
Matthias Fischmann <mf@zerobuzz.net>
2015-10-27 09:57:51 +0100

    Cleanup: whitespace.

--- thentos-tests/tests/Thentos/Backend/Api/SimpleSpec.hs
+++ thentos-tests/tests/Thentos/Backend/Api/SimpleSpec.hs
@@ -55,106 +55,103 @@ godHeaders = unsafePerformIO $ newIORef []
 
 spec :: Spec
 spec = do
-
     describe "Thentos.Backend.Api.Simple" . with defaultApp $ do
         specRest
         specPurescript
 
 specRest :: SpecWith Application
 specRest= do
-        describe "headers" $ do
-            it "bad unknown headers matching /X-Thentos-*/ yields an error response." $ do
+    describe "headers" $ do
+        it "bad unknown headers matching /X-Thentos-*/ yields an error response." $ do
+            hdr <- liftIO ctHeader
+            let headers = ("X-Thentos-No-Such-Header", "3"):hdr
+            request "GET" "/user/0/email" headers "" `shouldRespondWith` 400
+
+    describe "user" $ do
+        describe "Capture \"userid\" UserId :> \"name\" :> Get UserName" $ do
+            let resource = "/user/0/name"
+            it "yields a name" $ do
                 hdr <- liftIO ctHeader
-                let headers = ("X-Thentos-No-Such-Header", "3"):hdr
-                request "GET" "/user/0/email" headers "" `shouldRespondWith` 400
+                request "GET" resource hdr "" `shouldRespondWith` "\"god\""
 
-        describe "user" $ do
-            describe "Capture \"userid\" UserId :> \"name\" :> Get UserName" $ do
-                let resource = "/user/0/name"
-                it "yields a name" $ do
-                    hdr <- liftIO ctHeader
-                    request "GET" resource hdr "" `shouldRespondWith` "\"god\""
+            it "can be called by user herself" $
+                    \ _ -> pendingWith "test missing."
 
-                it "can be called by user herself" $
-                        \ _ -> pendingWith "test missing."
+            it "can be called by admin" $ do
+                hdr <- liftIO ctHeader
+                request "GET" resource hdr "" `shouldRespondWith` 200
 
-                it "can be called by admin" $ do
-                    hdr <- liftIO ctHeader
-                    request "GET" resource hdr "" `shouldRespondWith` 200
+            it "can not be called by other (non-admin) users" $
+                    \ _ -> pendingWith "test missing."
 
-                it "can not be called by other (non-admin) users" $
-                        \ _ -> pendingWith "test missing."
+        describe "Capture \"userid\" UserId :> \"email\" :> Get UserEmail" $ do
+            let resource = "/user/0/email"
+            it "yields an email address" $ do
+                hdr <- liftIO ctHeader
+                request "GET" resource hdr "" `shouldRespondWith` 200
 
-            describe "Capture \"userid\" UserId :> \"email\" :> Get UserEmail" $ do
-                let resource = "/user/0/email"
-                it "yields an email address" $ do
-                    hdr <- liftIO ctHeader
-                    request "GET" resource hdr "" `shouldRespondWith` 200
+        describe "ReqBody UserFormData :> Post UserId" $ do
+            it "writes a new user to the database" $ do
+                hdr <- liftIO ctHeader
+                response1 <- postDefaultUser
+                return response1 `shouldRespondWith` 201
 
-            describe "ReqBody UserFormData :> Post UserId" $ do
-                it "writes a new user to the database" $ do
-                    hdr <- liftIO ctHeader
-                    response1 <- postDefaultUser
-                    return response1 `shouldRespondWith` 201
+                let (uid :: Int) = read . cs $ simpleBody response1
+                response2 <- request "GET" ("/user/" <> (cs . show $ uid) <> "/name") hdr ""
 
-                    let (uid :: Int) = read . cs $ simpleBody response1
-                    response2 <- request "GET" ("/user/" <> (cs . show $ uid) <> "/name") hdr ""
+                let Right name = decodeLenient $ simpleBody response2
+                liftIO $ name `shouldBe` udName defaultUserData
 
-                    let Right name = decodeLenient $ simpleBody response2
-                    liftIO $ name `shouldBe` udName defaultUserData
+            it "can only be called by admins" $
+                    \ _ -> pendingWith "test missing."
 
-                it "can only be called by admins" $
-                        \ _ -> pendingWith "test missing."
+        describe "Capture \"userid\" UserId :> Delete" $ do
+            it "removes an existing user from the database" $ do
+                hdr <- liftIO ctHeader
+                response1 <- postDefaultUser
+                let (uid :: Int) = read . cs $ simpleBody response1
+                request "GET" ("/user/" <> (cs . show $ uid) <> "/name") hdr ""
+                    `shouldRespondWith` 200
+                void $ request "DELETE" ("/user/" <> cs (show uid)) hdr ""
+                request "GET" ("/user/" <> cs (show uid) <> "/name") hdr ""
+                    `shouldRespondWith` 404
 
-            describe "Capture \"userid\" UserId :> Delete" $ do
-                it "removes an existing user from the database" $ do
-                    hdr <- liftIO ctHeader
-                    response1 <- postDefaultUser
-                    let (uid :: Int) = read . cs $ simpleBody response1
-                    request "GET" ("/user/" <> (cs . show $ uid) <> "/name") hdr ""
-                        `shouldRespondWith` 200
-                    void $ request "DELETE" ("/user/" <> cs (show uid)) hdr ""
-                    request "GET" ("/user/" <> cs (show uid) <> "/name") hdr ""
-                        `shouldRespondWith` 404
+            it "can only be called by admins and the user herself" $
+                    \ _ -> pendingWith "test missing."
 
-                it "can only be called by admins and the user herself" $
-                        \ _ -> pendingWith "test missing."
+            it "if user does not exist, responds with a 404" $ do
+                hdr <- liftIO ctHeader
+                request "DELETE" "/user/1797" hdr "" `shouldRespondWith` 404
 
-                it "if user does not exist, responds with a 404" $ do
-                    hdr <- liftIO ctHeader
-                    request "DELETE" "/user/1797" hdr "" `shouldRespondWith` 404
+    describe "thentos_session" $ do
+        describe "ReqBody '[JSON] ThentosSessionToken :> Get Bool" $ do
+            it "returns true if session is active" $ do
+                hdr <- liftIO ctHeader
+                response1 <- postDefaultUser
+                let uid = read . cs $ simpleBody response1
+                response2 <- request "POST" "/thentos_session" hdr $
+                    Aeson.encode $ ByUser (UserId uid, udPassword defaultUserData)
+                request "GET" "/thentos_session/" hdr (simpleBody response2)
+                    `shouldRespondWith` "true" { matchStatus = 200 }
 
-        describe "thentos_session" $ do
-
-            describe "ReqBody '[JSON] ThentosSessionToken :> Get Bool" $ do
-                it "returns true if session is active" $ do
-                    hdr <- liftIO ctHeader
-                    response1 <- postDefaultUser
-                    let uid = read . cs $ simpleBody response1
-                    response2 <- request "POST" "/thentos_session" hdr $
-                        Aeson.encode $ ByUser (UserId uid, udPassword defaultUserData)
-                    request "GET" "/thentos_session/" hdr (simpleBody response2)
-                        `shouldRespondWith` "true" { matchStatus = 200 }
-
-                it "returns false if session is does not exist" $ do
-                    void postDefaultUser
-                    hdr <- liftIO ctHeader
-                    request "GET" "/thentos_session/" hdr (Aeson.encode ("x" :: ThentosSessionToken))
-                        `shouldRespondWith` "false" { matchStatus = 200 }
+            it "returns false if session is does not exist" $ do
+                void postDefaultUser
+                hdr <- liftIO ctHeader
+                request "GET" "/thentos_session/" hdr (Aeson.encode ("x" :: ThentosSessionToken))
+                    `shouldRespondWith` "false" { matchStatus = 200 }
 
 
 specPurescript :: SpecWith Application
 specPurescript = do
-        describe "purescript" $ do
-            describe "/js/*.js" $ do
+    describe "purescript" $ do
+        describe "/js/*.js" $ do
+            it "is available" $ do
+                request "GET" "/js/thentos.js" [] ""
+                    `shouldRespondWith` 200
 
-                it "is available" $ do
-                    request "GET" "/js/thentos.js" [] ""
-                        `shouldRespondWith` 200
-
-                it "has the right content type" $ do
-                    resp <- request "GET" "/js/thentos.js" [] ""
-                    liftIO $ cs (simpleBody resp) `shouldContain` ("PS[\"Main\"].main();" :: String)
+            it "has the right content type" $ do
+                resp <- request "GET" "/js/thentos.js" [] ""
+                liftIO $ cs (simpleBody resp) `shouldContain` ("PS[\"Main\"].main();" :: String)
 
 
 postDefaultUser :: WaiSession SResponse
