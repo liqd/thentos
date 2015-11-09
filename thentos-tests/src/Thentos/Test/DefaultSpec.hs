6661eb70929868fd26cebfd2c88a7ad79b0bc6b6
Matthias Fischmann <mf@zerobuzz.net>
2015-11-09 17:08:51 +0100

    Remove bogus pending test.

--- thentos-tests/src/Thentos/Test/DefaultSpec.hs
+++ thentos-tests/src/Thentos/Test/DefaultSpec.hs
@@ -17,7 +17,7 @@ import Control.Monad.State (liftIO)
 import Network.HTTP.Types.Status ()
 import Network.Wai (Application)
 import Network.Wai.Test (simpleBody)
-import Test.Hspec (SpecWith, describe, it, shouldSatisfy, pendingWith)
+import Test.Hspec (SpecWith, describe, it, shouldSatisfy)
 import Test.Hspec.Wai (shouldRespondWith, get)
 
 import qualified Data.ByteString.Lazy as LBS
@@ -31,8 +31,3 @@ specHasRestDocs = do
         it "has markdown" $ do
             get "/docs/md" `shouldRespondWith` 200
             get "/docs/md" >>= bodyNonEmpty
-
-        it "has html" $ do
-            liftIO $ pendingWith "we don't want to depend on pandoc to render this internally."
-            get "/docs/html" `shouldRespondWith` 200
-            get "/docs/html" >>= bodyNonEmpty
