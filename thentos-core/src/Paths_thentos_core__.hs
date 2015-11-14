f4531fe006362e5cd381371616b9f8db4a99663b
Matthias Fischmann <mf@zerobuzz.net>
2015-11-14 12:54:09 +0100

    ...

--- thentos-core/src/Paths_thentos_core__.hs
+++ thentos-core/src/Paths_thentos_core__.hs
@@ -1,13 +1,16 @@
 {-# LANGUAGE CPP             #-}
 #if DEVELOPMENT
+#warning "building in development mode (-fdevelopment)"
 {-# LANGUAGE TemplateHaskell #-}
+#else
+#warning "building in production mode: (-f-development)"
 #endif
 
 -- | Custom modifications to "Paths_thentos_core".
 module Paths_thentos_core__ (getDataFileName, getBuildRootDirectory, version) where
 
 -- | use this only for testing or build-time effects!
-import Paths.TH (getBuildRootDirectory)
+import Paths.TH (getPackageSourceRoot)
 
 #if !DEVELOPMENT
 import Paths_thentos_core
@@ -17,7 +20,7 @@ import Distribution.Version (Version(Version))
 import System.FilePath ((</>))
 
 getDataFileName :: FilePath -> IO FilePath
-getDataFileName = return . (($(getBuildRootDirectory) </> "thentos-core") </>)
+getDataFileName = return . ($(getPackageSourceRoot) </>)
 
 version :: Version
 version = Version [] []
