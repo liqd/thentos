1de3166c1ce26169c1e2057c08b25a234a45ce79
Matthias Fischmann <mf@zerobuzz.net>
2015-11-27 09:11:16 +0100

    Fix: clean up foreign objects before returning (required for termination).

--- thentos-purescript/src/Register.js
+++ thentos-purescript/src/Register.js
@@ -4,6 +4,24 @@
 // module Register
 exports.eventInputValue = function(ev) {
     return { value: ev.target.value,
-             validity: ev.target.validty }
+             validity: marshal(ev.target.validity) };
 
+};
+
+// ev.target.validity is an rich object, not just a json dictionary.
+// i think.  anyway, this function turns it into something that can be
+// passed into purescript via the foreign function binding.
+var marshal = function(v) {
+    return {
+        valueMissing:    v.valueMissing
+      , typeMismatch:    v.typeMismatch
+      , patternMismatch: v.patternMismatch
+      , tooLong:         v.tooLong
+      , rangeUnderflow:  v.rangeUnderflow
+      , rangeOverflow:   v.rangeOverflow
+      , stepMismatch:    v.stepMismatch
+      , badInput:        v.badInput
+      , customError:     v.customError
+      , valid:           v.valid
+    };
 };
