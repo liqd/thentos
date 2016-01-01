2524f6f4f4f4bf33213224b830157aa32c73e129
Matthias Fischmann <mf@zerobuzz.net>
2016-01-01 13:03:47 +0100

    ...

--- thentos-core/src/Thentos/Util.hs
+++ thentos-core/src/Thentos/Util.hs
@@ -1,5 +1,9 @@
 {-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TupleSections       #-}
 {-# LANGUAGE OverloadedStrings   #-}
+
+{-# OPTIONS_GHC -fno-warn-unused-binds #-}
 
 module Thentos.Util
     ( hashUserPass
@@ -19,6 +23,7 @@ import Control.Lens ((^.))
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Control.Monad.Trans.Either (EitherT(EitherT), runEitherT)
 import Data.String.Conversions (ConvertibleStrings, SBS, ST, cs)
+import Data.List
 import Data.Text.Encoding (encodeUtf8)
 import Network.HTTP.Types (urlEncode)
 
@@ -26,6 +31,82 @@ import qualified Crypto.Scrypt as Scrypt
 import qualified Data.Text as ST
 
 import Thentos.Types
+
+
+-- * scratch
+
+import Crypto.BCrypt
+import Control.Monad
+import Data.Text.Encoding
+import Data.Text.ICU.Convert
+import Data.Functor.Infix ((<$$>))
+
+-- the puzzle: go to the repo root and run `make pytest`.  you will get a pair of plaintext and
+-- bcrypt-protected password in the fashion that works for a3.  now get hs-bcrypt as called in the
+-- following lines to verify that pair successfully.
+--
+-- related: https://github.com/liqd/adhocracy3.mercator/pull/1808 is about a script that exports the
+-- data from adhocracy3.  in master: ./bin/export_mercator_users;
+-- ./src/adhocracy_mercator/adhocracy_mercator/scripts/export_users.py.  `make pytest` has been
+-- checked to generate compatible data with what the script extracts.
+
+good :: (SBS, SBS)
+good = ("wefwefwef", "$2a$10$VgGCKVJAyHBTtflKihqGC.fGJ5j7sIZwptAKTfBPsjygS3xjzb1a2")
+
+bad :: (SBS, SBS)
+bad = ("incorrect", "$2a$10$KLJDd7d6VCKSIPcDA3QHGu9tYpfnOTSvgueXu/JmwlxfWY4fDOAlW")
+
+-- run a test pair of plain and protected string through hs-bcrypt on all possible encodings.
+-- prints a boolean that states whether the pair was good (plaintext matches protected string in
+-- some encoding).
+mainTest :: (SBS, SBS) -> IO ()
+mainTest (m, c) = do
+  ms <- allEncodings m
+  let valid = or $ (`Crypto.BCrypt.validatePassword` c) <$> ms
+  print valid
+
+-- come up with as many encodings as i could think of.  one of them should be the one used in
+-- cryptacular.
+allEncodings :: SBS -> IO [SBS]
+allEncodings m = do
+    let easy = [ m
+               , encodeUtf8 $ cs m
+               , encodeUtf16LE $ cs m
+               , encodeUtf16BE $ cs m
+               , encodeUtf32LE $ cs m
+               , encodeUtf32BE $ cs m
+               ]
+    icu <- forM converterNames $ \cnvnm -> (\cnv -> fromUnicode cnv $ cs m) <$> open cnvnm Nothing
+    -- print $ length $ nub icu  -- ==> 17
+    return $ easy ++ icu
+
+main :: IO ()
+main = do
+  putStrLn "should be True-False:"
+  mainTest good
+  mainTest bad
+
+hp :: HashingPolicy
+hp = HashingPolicy 10 "$2a$"
+
+-- look at what hs-bcrypt does in all encodings.  (not very informative, as salting should destroy
+-- all patterns should there be any.)
+q :: IO ()
+q = do
+    xs :: [SBS] <- allEncodings $ fst good
+    ys <- forM xs $ hashPasswordUsingPolicy hp
+    mapM_ print $ {- filter (== Just (snd good)) . -} nub . sort $ ys
+
+
+
+-- once this has worked out: change the 'verifyPass' below such that it accepts both scrypt and
+-- bcrypt-encoded passwords (the latter as delivered by a3).  confirm that checking both in turn is
+-- not a significant performance penalty (i think if the password does not match, at least bcrypt is
+-- very fast).
+
+
+
+
 
 
 -- * crypto
