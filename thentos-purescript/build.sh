ec3a0fd9ceb692f35491dda15fcb56fe517e5b9f
Matthias Fischmann <mf@zerobuzz.net>
2016-01-05 10:36:09 +0100

    Cleanup.

--- thentos-purescript/build.sh
+++ thentos-purescript/build.sh
@@ -6,19 +6,6 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 export PATH=`pwd`/node_modules/.bin/:$PATH
 
 case "$1" in
-    "pull-cache")
-        test "$2" == "" && ( echo "$0: please specify cache path."; exit 1 )
-        echo "pulling build cache from $2..."
-        rsync -a --delete "$2"/bower_components . || true
-        rsync -a --delete "$2"/node_modules .     || true
-        ;;
-    "push-cache")
-        test "$2" == "" && ( echo "$0: please specify cache path."; exit 1 )
-        echo "pushing build cache to $2..."
-        mkdir -p "$2"
-        rsync -a --delete bower_components "$2"
-        rsync -a --delete node_modules "$2"
-        ;;
     "dep")
         echo -n 'node: ';  node  --version
         echo -n 'npm: ';   npm   --version
@@ -40,6 +27,19 @@ case "$1" in
     "distclean")
         $0 clean
         rm -rf ./bower_components ./node_modules
+        ;;
+    "pull-cache")
+        test "$2" == "" && ( echo "$0: please specify cache path."; exit 1 )
+        echo "pulling build cache from $2..."
+        rsync -a --delete "$2"/bower_components . || true
+        rsync -a --delete "$2"/node_modules .     || true
+        ;;
+    "push-cache")
+        test "$2" == "" && ( echo "$0: please specify cache path."; exit 1 )
+        echo "pushing build cache to $2..."
+        mkdir -p "$2"
+        rsync -a --delete bower_components "$2"
+        rsync -a --delete node_modules "$2"
         ;;
     *)
         echo "usage: $0 [dep|it|watch|clean|distclean|pull-cache|push-cache]" >&2
