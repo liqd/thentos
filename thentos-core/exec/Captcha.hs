8924c7f0a5f030f21ec4c99aa8b3551058caa215
Matthias Fischmann <mf@zerobuzz.net>
2016-01-08 11:15:53 +0100

    How to build different executables.
    - src/Thentos.hs is the generic Main module
    - things to be provided to lib users and specifics to more than one executables go to src/Thentos.hs
    - specifics to one executable go to the resp. executable module in exec/

--- thentos-core/exec/Captcha.hs
+++ thentos-core/exec/Captcha.hs
@@ -1,6 +1,41 @@
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE PackageImports             #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE ViewPatterns               #-}
+
 module Main (main) where
 
-import qualified Thentos.CaptchaService
+import Control.Exception (finally)
+import Data.Configifier ((>>.), Tagged(Tagged))
+import Data.Proxy (Proxy(Proxy))
+import System.Log.Logger (Priority(INFO), removeAllHandlers)
+
+import System.Log.Missing (logger, announceAction)
+import Thentos (createConnPoolAndInitDb, runGcLoop, makeActionState)
+import Thentos.Config
+import Thentos.Smtp (checkSendmail)
+
+import qualified Thentos.Backend.Api.Captcha as Captcha
+
 
 main :: IO ()
-main = Thentos.CaptchaService.main
+main = do
+    config :: ThentosConfig <- getConfig "devel.config"
+    -- FIXME checkEspeak instead
+    checkSendmail (Tagged $ config >>. (Proxy :: Proxy '["smtp"]))
+
+    connPool <- createConnPoolAndInitDb $ config >>. (Proxy :: Proxy '["database", "name"])
+    actionState <- makeActionState config connPool
+    configLogger . Tagged $ config >>. (Proxy :: Proxy '["log"])
+
+    _ <- runGcLoop actionState $ config >>. (Proxy :: Proxy '["gc_interval"])
+
+    case Tagged <$> config >>. (Proxy :: Proxy '["backend"]) of
+        Nothing -> return () -- TODO: error message
+        Just backendConfig -> do
+            logger INFO "Press ^C to abort."
+            let run = Captcha.runApi backendConfig actionState
+                finalize = announceAction "shutting down hslogger" removeAllHandlers
+
+            run `finally` finalize
