39edc44774c30910c60c5dd2c3835ea23d4e2506
Matthias Fischmann <mf@zerobuzz.net>
2016-01-11 07:53:24 +0100

    CORS for captcha api.

--- thentos-core/src/Thentos/Backend/Api/Captcha.hs
+++ thentos-core/src/Thentos/Backend/Api/Captcha.hs
@@ -1,26 +1,37 @@
 {-# LANGUAGE DataKinds             #-}
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE KindSignatures        #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE PolyKinds             #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TypeFamilies          #-}
 {-# LANGUAGE TypeOperators         #-}
 
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Thentos.Backend.Api.Captcha where
 
-import Control.Monad (when)
+import Control.Lens ((&), (%~), (.~))
 import Control.Monad.Except (catchError, throwError)
+import Control.Monad (when)
 import Data.Proxy (Proxy(Proxy))
 import Data.String.Conversions (ST, SBS, cs)
 import Data.Void (Void)
-import Servant.API.Header (Header)
+import Network.HTTP.Types (methodOptions)
+import Network.HTTP.Types.Status (ok200)
 import Network.Wai (Application)
+import Servant.API (GetHeaders)
+import Servant.API.Header (Header)
 import Servant.API ((:<|>)((:<|>)), (:>), Post, Capture, ReqBody, JSON)
-import Servant.Server (ServerT, Server, serve, enter)
 import Servant.API.ResponseHeaders (Headers, addHeader)
+import Servant.Server (HasServer(route), ServerT, Server, serve, enter)
+import Servant.Server.Internal (methodRouterHeaders)
 import System.Log.Logger (Priority(INFO))
 
 import qualified Servant.Docs as Docs
+import qualified Servant.Foreign.Internal as Foreign
 
 import System.Log.Missing (logger)
 import Thentos.Action
@@ -55,24 +66,61 @@ api as = \creds -> enter (enterAction () as baseActionErrorToServantErr creds) t
 
 -- * captcha
 
-type CaptchaHeaders a = Headers '[Header "Thentos-Captcha-Id" CaptchaId] a
+type CaptchaOptionsHeaders =
+    '[ Header "Access-Control-Allow-Origin" ST
+     , Header "Access-Control-Expose-Headers" ST
+     , Header "Access-Control-Max-Age" ST
+     , Header "Access-Control-Allow-Credentials" ST
+     , Header "Access-Control-Allow-Methods" ST
+     , Header "Access-Control-Allow-Headers" ST
+     ]
 
-type ThentosCaptcha =
-       "captcha"                             :> Post '[PNG] (CaptchaHeaders ImageData)
-  :<|> "audio_captcha" :> Capture "voice" ST :> Post '[WAV] (CaptchaHeaders SBS)
-  :<|> "solve_captcha" :>  ReqBody '[JSON] CaptchaSolution :> Post '[JSON] (JsonTop Bool)
+addCaptchaOptionsHeaders :: a -> Headers CaptchaOptionsHeaders a
+addCaptchaOptionsHeaders a = a
+     & addHeader "*"
+     & addHeader "X-Thentos-Captcha-Id"
+     & addHeader "900"
+     & addHeader "true"
+     & addHeader "POST,GET,DELETE,PUT,OPTIONS"
+     & addHeader "Origin, Content-Type, Accept, X-User-Path, X-User-Token"
+
+type CaptchaHeaders = Header "X-Thentos-Captcha-Id" CaptchaId ': CaptchaOptionsHeaders
+
+addCaptchaHeaders :: CaptchaId -> a -> Headers CaptchaHeaders a
+addCaptchaHeaders cid = addHeader cid . addCaptchaOptionsHeaders
+
+type ThentosCaptcha = ThentosCaptchaFrontend :<|> ThentosCaptchaBackend
+
+type ThentosCaptchaFrontend =
+       "captcha"                             :> Options (Headers CaptchaOptionsHeaders ())
+  :<|> "captcha"                             :> Post    '[PNG] (Headers CaptchaHeaders ImageData)
+  :<|> "audio_captcha" :> Capture "voice" ST :> Options (Headers CaptchaOptionsHeaders ())
+  :<|> "audio_captcha" :> Capture "voice" ST :> Post    '[WAV] (Headers CaptchaHeaders SBS)
+
+type ThentosCaptchaBackend =
+       "solve_captcha" :> ReqBody '[JSON] CaptchaSolution :> Post '[JSON] (JsonTop Bool)
 
 thentosCaptcha :: ServerT ThentosCaptcha (Action Void ())
-thentosCaptcha =
-       captchaImgH
+thentosCaptcha = thentosCaptchaFrontend :<|> thentosCaptchaBackend
+
+thentosCaptchaFrontend :: ServerT ThentosCaptchaFrontend (Action Void ())
+thentosCaptchaFrontend =
+       preflightH
+  :<|> captchaImgH
+  :<|> (\_ -> preflightH)
   :<|> captchaWavH
-  :<|> captchaSolveH
 
-captchaImgH :: Action Void () (CaptchaHeaders ImageData)
-captchaImgH = (\(cid, img) -> addHeader cid img) <$> makeCaptcha
+thentosCaptchaBackend :: ServerT ThentosCaptchaBackend (Action Void ())
+thentosCaptchaBackend = captchaSolveH
 
-captchaWavH :: ST -> Action Void () (CaptchaHeaders SBS)
-captchaWavH voice = (\(cid, wav) -> addHeader cid wav) <$> makeAudioCaptcha (cs voice)
+preflightH :: Action Void () (Headers CaptchaOptionsHeaders ())
+preflightH = pure $ addCaptchaOptionsHeaders ()
+
+captchaImgH :: Action Void () (Headers CaptchaHeaders ImageData)
+captchaImgH = (\(cid, img) -> addCaptchaHeaders cid img) <$> makeCaptcha
+
+captchaWavH :: ST -> Action Void () (Headers CaptchaHeaders SBS)
+captchaWavH voice = (\(cid, wav) -> addCaptchaHeaders cid wav) <$> makeAudioCaptcha (cs voice)
 
 captchaSolveH :: CaptchaSolution -> Action Void () (JsonTop Bool)
 captchaSolveH (CaptchaSolution cid solution) = JsonTop <$> do
@@ -98,3 +146,26 @@ instance HasDocExtras (RestDocs Api) where
             [ "A lean service that provides visual and audio captchas and verifies whether"
             , "submitted solutions are correct."
             ]]]
+
+
+-- * OPTIONS verb
+
+-- FIXME: this should go elsewhere, ideally to servant, but we only need it here for now.
+-- FIXME: not sure if the content type list applies here, as body is always empty.
+
+data Options a
+
+instance {-# OVERLAPPABLE #-} ( GetHeaders (Headers h ()) )
+        => HasServer (Options (Headers h ())) where
+    type ServerT (Options (Headers h ())) m = m (Headers h ())
+    route Proxy = methodRouterHeaders methodOptions (Proxy :: Proxy '[JSON]) ok200
+
+instance {-# OVERLAPPABLE #-} Foreign.HasForeign Foreign.NoTypes
+        (Options (Headers CaptchaOptionsHeaders ())) where
+    type Foreign (Options (Headers CaptchaOptionsHeaders ())) = Foreign.Req
+    foreignFor Proxy Proxy req =
+        req & Foreign.funcName  %~ ("options" :)
+            & Foreign.reqMethod .~ "OPTIONS"
+
+instance Docs.HasDocs (Options (Headers CaptchaOptionsHeaders ())) where
+    docsFor _ dat opts = mempty  -- FIXME: be more helpful here?
