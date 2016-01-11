8a7c50f07a45b7dbeff122a10727c8ce86c13eca
Scott Sadler <scott@alphanode.co>
2016-01-11 11:11:08 +0000

    * Put params closer together * Make biteLetter use constant offset... Random offset doesn't neccesarily work well * Constant size of 300*100 * Cleaner and more betterer

--- thentos-core/src/Thentos/Sybil/GraphicCaptcha.hs
+++ thentos-core/src/Thentos/Sybil/GraphicCaptcha.hs
@@ -29,47 +29,51 @@ generateCaptcha rnd = do
     font <- either error id <$> loadFontFile fontPath
     return (challenge font, cs solution)
   where
-    (solution, randoms) = flip evalRand (random20ToStdGen rnd)
-        ((,) <$> mkSolution <*> getRandoms)
+    solution = flip evalRand (random20ToStdGen rnd) mkSolution
     random20ToStdGen = mkStdGen . sum . map ord . cs . fromRandom20
-    challenge font = mkChallenge font randoms solution
+    challenge font = mkChallenge font solution
 
 
 mkSolution :: MonadRandom m => m String
 mkSolution = unwords <$> replicateM 2 (mkPassword 3)
 
 
-mkChallenge :: Font -> [Float] -> String -> ImageData
-mkChallenge font rnds solution =
-    let w = 500
-        h = 100
-        render = renderDrawing w h (PixelRGBA8 255 255 255 255)
-        letterParams = zip3 [20,70..] solution rnds
+mkChallenge :: Font -> String -> ImageData
+mkChallenge font solution =
+    let width = 300
+        height = 100
+        offsets = [30,65..]
+        yOffset = 30
+        textPx = 40
+        render = renderDrawing width height (PixelRGBA8 255 255 255 255)
+        letterParams = zip offsets solution
     in ImageData $ cs $ encodePng $ render $
-        forM_ letterParams $ \(offset, char, rnd) ->
-             withTransformation (translate $ V2 offset 20) $
-                 biteLetter font 40 rnd char
+        forM_ letterParams $ \(offset, char) ->
+             withTransformation (translate $ V2 offset yOffset) $
+                 biteLetter font textPx char
 
 
 -- | Render a letter with a chunk displaced.
-biteLetter :: Font -> Float -> Float -> Char -> Drawing PixelRGBA8 ()
-biteLetter _ _ _ ' ' = return ()
-biteLetter font size rnd char = do
+biteLetter :: Font -> Float -> Char -> Drawing PixelRGBA8 ()
+biteLetter _ _ ' ' = return ()
+biteLetter font size char = do
     withTexture texLetter $ fill rect
-    withTexture (biteShift texLetter) $ fill bite
+    withTexture texWhite $ fill hole
+    withTexture texChunk $ fill chunk
   where
     -- Render letter to texture
     black = PixelRGBA8 0 0 0 255
     white = PixelRGBA8 255 255 255 255
+    transparent = PixelRGBA8 255 255 255 0
     texBlack = uniformTexture black
+    texWhite = uniformTexture white
     printText = printTextAt font (PointSize size) (V2 0 size) [char]
     letter = withTexture texBlack printText
-    texLetter = patternTexture 100 100 96 white letter
+    texLetter = patternTexture 100 100 96 transparent letter
     rect = rectangle (V2 0 0) size 50
     -- Take a chunk of the letter and displace it randomly
-    radius = 20
-    biteX = rnd * radius
-    biteOffset = V2 biteX (radius - biteX)
-    bite = rectangle biteOffset 20 20
-    biteShift = transformTexture $ translate (biteOffset ^/ 2)
-
+    holeOffset = V2 10 0
+    hole = rectangle holeOffset 30 30
+    chunkOffset = V2 (-10) (-5)
+    chunk = rectangle chunkOffset 30 30
+    texChunk = transformTexture (translate $ chunkOffset ^* (-1)) texLetter
