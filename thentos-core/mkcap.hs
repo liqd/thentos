273ee7c206501badffb0b9e58bfcad56ebf11d44
Scott Sadler <scott@alphanode.co>
2016-01-12 13:09:19 +0000

    bump version

---
+++ thentos-core/mkcap.hs
@@ -0,0 +1,20 @@
+
+module Mkcap (img) where
+
+import Control.Monad (forM_)
+import Data.ByteString as BS
+import Data.String.Conversions
+import Thentos.Sybil
+import Thentos.Types
+
+
+img :: IO ()
+img = do
+    let baseNum = 11234597890123456789
+    forM_ [0..20] $ \n -> do
+        let Just rnd = mkRandom20 $ cs $ show $ baseNum + n
+        (ImageData pngData, solution) <- generateCaptcha rnd
+        let path = "/host/tmp/" ++ cs solution ++ ".png"
+        BS.writeFile path pngData
+
+
