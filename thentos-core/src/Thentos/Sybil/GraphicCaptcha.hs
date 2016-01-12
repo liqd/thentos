04b7692418c75241ac619df30a54635ab765a453
Matthias Fischmann <mf@zerobuzz.net>
2016-01-12 16:46:53 +0100

    Make captcha bites more random.

--- thentos-core/src/Thentos/Sybil/GraphicCaptcha.hs
+++ thentos-core/src/Thentos/Sybil/GraphicCaptcha.hs
@@ -1,10 +1,11 @@
 {-# LANGUAGE OverloadedStrings    #-}
+{-# LANGUAGE ScopedTypeVariables  #-}
 
 module Thentos.Sybil.GraphicCaptcha (generateCaptcha) where
 
 import Control.Exception (throwIO, ErrorCall(ErrorCall))
-import Control.Monad.Except (replicateM, forM_)
-import Control.Monad.Random.Class (MonadRandom)
+import Control.Monad.Except (replicateM, forM_, forM)
+import Control.Monad.Random.Class (MonadRandom, getRandomR)
 import Control.Monad.Random (mkStdGen, evalRand)
 import Data.Char (ord)
 import Data.Elocrypt (mkPassword)
@@ -12,7 +13,7 @@ import Data.String.Conversions (ST, cs)
 
 import Codec.Picture (PixelRGBA8(PixelRGBA8), encodePng)
 import Graphics.Rasterific
-    ( Drawing, PointSize(PointSize)
+    ( Drawing, PointSize(PointSize), Point
     , withTransformation, withTexture, printTextAt, rectangle, renderDrawing, fill
     )
 import Graphics.Rasterific.Linear (V2(V2), (^*))
@@ -30,18 +31,17 @@ generateCaptcha :: Random20 -> IO (ImageData, ST)
 generateCaptcha rnd = do
     fontPath <- getDataFileName "resources/fonts/Courier_Prime_Bold.ttf"
     font <- loadFontFile fontPath >>= either (throwIO . ErrorCall) return
-    return (challenge font, cs solution)
-  where
-    solution = evalRand mkSolution (random20ToStdGen rnd)
-    random20ToStdGen = mkStdGen . sum . map ord . cs . fromRandom20
-    challenge font = mkChallenge font solution
-
+    let random20ToStdGen = mkStdGen . sum . map ord . cs . fromRandom20
+    return $ flip evalRand (random20ToStdGen rnd) $ do
+        solution <- mkSolution
+        challenge <- mkChallenge font solution
+        return (challenge, cs solution)
 
 mkSolution :: MonadRandom m => m String
 mkSolution = unwords <$> replicateM 2 (mkPassword 3)
 
 
-mkChallenge :: Font -> String -> ImageData
+mkChallenge :: forall m. MonadRandom m => Font -> String -> m ImageData
 mkChallenge font solution =
     let width = 300
         height = 100
@@ -50,16 +50,30 @@ mkChallenge font solution =
         textPx = 40
         render = renderDrawing width height (PixelRGBA8 255 255 255 255)
         letterParams = zip offsets solution
-    in ImageData $ cs $ encodePng $ render $
-        forM_ letterParams $ \(offset, char) ->
-            withTransformation (translate $ V2 offset yOffset) $
-                biteLetter font textPx char
+
+        fuzz :: Float -> m Float
+        fuzz i = getRandomR (i * 0.7 + 0.2, i * 1.3 - 0.2)
+
+        action :: m (Drawing PixelRGBA8 ())
+        action = do
+            offsetParams <- forM solution $ \_ -> do
+                holeOffset@(V2 x y) <- V2 <$> fuzz 10 <*> fuzz 0
+                chunkOffset         <- V2 <$> fuzz (-x) <*> fuzz (-5-y)
+                return (holeOffset, chunkOffset)
+
+            let allParams = zip letterParams offsetParams
+
+            return $ forM_ allParams $ \((offset, char), (holeOffset, chunkOffset)) -> do
+                withTransformation (translate $ V2 offset yOffset) $
+                    biteLetter holeOffset chunkOffset font textPx char
+
+    in ImageData . cs . encodePng . render <$> action
 
 
 -- | Render a letter with a chunk displaced.
-biteLetter :: Font -> Float -> Char -> Drawing PixelRGBA8 ()
-biteLetter _ _ ' ' = return ()
-biteLetter font size char = do
+biteLetter :: Point -> Point -> Font -> Float -> Char -> Drawing PixelRGBA8 ()
+biteLetter _ _ _ _ ' ' = return ()
+biteLetter holeOffset chunkOffset font size char = do
     withTexture texLetter $ fill rect
     withTexture texWhite $ fill hole
     withTexture texChunk $ fill chunk
@@ -75,8 +89,6 @@ biteLetter font size char = do
     texLetter = patternTexture 100 100 96 transparent letter
     rect = rectangle (V2 0 0) size 50
     -- Take a chunk of the letter and displace it randomly
-    holeOffset = V2 10 0
     hole = rectangle holeOffset 30 30
-    chunkOffset = V2 (-10) (-5)
     chunk = rectangle chunkOffset 30 30
     texChunk = transformTexture (translate $ chunkOffset ^* (-1)) texLetter
