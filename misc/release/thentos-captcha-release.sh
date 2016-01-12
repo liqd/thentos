479abb0c1416067719f72a17830cab8a810b7e47
Scott Sadler <scott@alphanode.co>
2016-01-12 11:23:21 +0000

    update readme and copy correct config

--- misc/release/thentos-captcha-release.sh
+++ misc/release/thentos-captcha-release.sh
@@ -49,7 +49,7 @@ cp $cabal_sandbox/bin/thentos-captcha .
 mkdir -p thentos-core/schema
 cp -r ../thentos/thentos-core/schema/* thentos-core/schema
 cp ../thentos/misc/release/thentos-captcha-README.md README.md
-#TODO: Copy config
+cp ../thentos/thentos-core/devel.config thentos-captcha.config
 cd ..
 tar cf $tar *
 gzip $tar
