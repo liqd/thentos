fbdaeda0b63888cf08ae423d52101639f2d014e0
Matthias Fischmann <mf@zerobuzz.net>
2016-04-19 06:47:43 +0200

    Errata.

--- thentos-core/src/Servant/Missing.hs
+++ thentos-core/src/Servant/Missing.hs
@@ -25,6 +25,7 @@ module Servant.Missing
   ,redirect) where
 
 import Control.Lens (prism, Prism', (#))
+import Control.Monad ((>=>))
 import Control.Monad.Except (MonadError, throwError)
 import Control.Monad.Except.Missing (finally)
 import Control.Monad.Identity (Identity, runIdentity)
@@ -160,8 +161,8 @@ formRedirectH :: forall payload m htm html uri.
   -> (payload -> m uri)              -- ^ processor2
   -> (View html -> uri -> m html)    -- ^ renderer
   -> ServerT (FormH htm html payload) m
-formRedirectH formAction processor1 processor2 renderer =
-    formH (Nat liftIO) formAction processor1 (\p -> processor2 p >>= redirect) renderer
+formRedirectH formAction processor1 processor2 =
+    formH (Nat liftIO) formAction processor1 (processor2 >=> redirect)
 
 
 redirect :: (MonadServantErr err m, ConvertibleStrings uri SBS) => uri -> m a
